7c854f96cfb114ec8d5aa2857b6224d8904bd820
Vishal Rindhe <rindhevishal32@gmail.com>
2021-03-01 09:56:28 +0530

    binding event by arrow function

--- src/components/eventBind.js
+++ src/components/eventBind.js
@@ -22,7 +22,9 @@ class eventBind extends Component {
         return (
             <div>
                 <h1>{this.state.message}</h1>
-                <button onClick={this.eventClick.bind(this)}>Click</button>
+                {/* <button onClick={this.eventClick.bind(this)}>Click</button> */}
+                <button onClick={() => this.eventClick()}>Click</button>
+
             </div>
         )
     }
