accc83cbce5b990dfdb0092d55ee4ebcec586f35
Vishal Rindhe <rindhevishal32@gmail.com>
2021-03-01 17:23:30 +0530

    used index position as key

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -3,32 +3,35 @@ import Person from './Person'
 
 function NameList() {
     
-    // const name = ['Abc', 'Pqr', 'Xyz']
+    const name = ['Abc', 'Pqr', 'Xyz']
 
-    const persons = [{
-        id : 1,
-        name : 'Abc',
-        age: 30,
-        tech : 'Angular'
-    },
-    {
-        id : 2,
-        name : 'Pqr',
-        age: 30,
-        tech : 'Angular'
-    },{
-        id : 3,
-        name : 'Xyz',
-        age: 30,
-        tech : 'Angular'
-    }]
+    // const persons = [{
+    //     id : 1,
+    //     name : 'Abc',
+    //     age: 30,
+    //     tech : 'Angular'
+    // },
+    // {
+    //     id : 2,
+    //     name : 'Pqr',
+    //     age: 30,
+    //     tech : 'Angular'
+    // },{
+    //     id : 3,
+    //     name : 'Xyz',
+    //     age: 30,
+    //     tech : 'Angular'
+    // }]
 
         // const personList = persons.map(person => <h2>I am {person.name} with Id no {person.id}</h2>)
-        const personList = persons.map(person => <Person key={person.name} person={person} />)
+        // const personList = persons.map(person => <Person key={person.name} person={person} />)
+        const nameList = name.map((name,index) => <h2 key={index}>{index}  {name}</h2>)
+
 
     return (
         <div>
-            {personList}
+            {/* {personList} */}
+            {nameList}
         </div>
     )
 }
